a64332fc5bae813c259963dbc57ee4b99d27e55c
Andres Gonzalez <andres.gonzamatamoros@gmail.com>
2025-09-29 18:35:58 -0500

    add tests/test-validateprod.spec.ts

---
+++ tests/test-validateprod.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test('validate inventory item', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/v1/');
+    await page.getByPlaceholder("Username").fill("standard_user");
+    await page.locator("[data-test='password']").fill("secret_sauce");
+    await page.getByRole('button', { name: "LOGIN" }).click();
+    await page.getByRole('link', { name: "Sauce Labs Bike Light" }).click();
+    await page.getByRole('button', { name: 'ADD TO CART' }).click();
+    await expect(page.locator(".shopping_cart_badge")).toHaveText('1')
+    await expect(page.getByText("Sauce Labs Bike Light")).toBeVisible();
+
+})
+
+test('eliminate inventory item', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/v1/');
+    await page.getByPlaceholder("Username").fill("standard_user");
+    await page.locator("[data-test='password']").fill("secret_sauce");
+    await page.getByRole('button', { name: "LOGIN" }).click();
+    await page.getByRole('link', { name: "Sauce Labs Bolt T-Shirt" }).click();
+    await page.getByRole('button', { name: 'ADD TO CART' }).click();
+    await expect(page.locator(".shopping_cart_badge")).toHaveText('1')
+    await page.goto('https://www.saucedemo.com/v1/cart.html');
+    await expect(page.getByText("Sauce Labs Bolt T-Shirt")).toBeVisible();
+    await page.getByRole('button', { name: 'REMOVE' }).click();
+    await expect(badge).toHaveCount(0);
+    await expect(cartItems).toHaveCount(0);
+
+
+
+})
